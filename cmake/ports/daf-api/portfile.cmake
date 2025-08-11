c55a59d62b6a6445345e54b4ba3e6657218efa43
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-08-11 15:44:42 +0200

    Update portfile.cmake

--- cmake/ports/daf-api/portfile.cmake
+++ cmake/ports/daf-api/portfile.cmake
@@ -2,8 +2,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO QTR-Modding/DynamicAnimationFrameworkSE_API
-    REF 0
-    SHA512 0
+    REF b88420430ba6efb8932a062c674f04fd2647737f
+    SHA512 bf2c9dcbcb3e40f5e88b001da9f0b9d2d3e048526a36286aae27274e26ad389ef38e419a7bccbbdf3d3446a870af4142f1bddbb1815ff62ee631a970fbc57e74
     HEAD_REF main
 )
 
